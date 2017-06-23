ff30ed7ca0b8b2fa11fe7906dc0ac353a285a220
RSijelmass <R.Sijelmass@gmail.com>
2017-06-23 14:27:57 +0100

    Remove console.log from interface

--- views/interface.js
+++ views/interface.js
@@ -17,7 +17,6 @@ document.addEventListener("DOMContentLoaded", function() {
 		var resultsHash = guardianApiJson["response"]["results"];
 	    for(var i = 0; i < resultsHash.length; i++) {
 				var newStory = new Story(resultsHash[i])
-				console.log(newStory.url);
 				var storyComponent = new StoryComponent(newStory);
 				storyComponent.render();
 	    }
