711770543817149dd4335748c0e6b93d731a8d94
RSijelmass <R.Sijelmass@gmail.com>
2017-06-23 10:29:59 +0100

    Gets Guardian API in interface - create getStories(), takes out api as string through xmlHttpRequest

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -6,7 +6,12 @@
     assert.hasContent('headline-list', story.headline);
   }
 
+	function displayApiHeadline() {
+		assert.hasContent('headline-list', "Juncker says May's EU citizens Brexit offer is 'not sufficient' - Politics live")
+	}	
+
   function runFeatureTests() {
     displayHeadline();
+		displayApiHeadline();
     console.log('Team Bearfoot : Feature Tests Passed');
   }
--- views/interface.js
+++ views/interface.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-var storyComponent = new StoryComponent(story);
+	var api = "http://content.guardianapis.com/search?api-key=test"
+	debugger
 
- renderStory = function(storyComponent) {
+	function getStories(api) {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("GET", api, false);
+		xmlHttp.send(null);
+		return xmlHttp.responseText;
+	}
+		
+	var apiString = getStories(api);
+	
+
+	var storyComponent = new StoryComponent(story);
+
+ 	renderStory = function(storyComponent) {
  		storyComponent.render();
- }
+ 	}
 
- renderStory(storyComponent);
+ 	renderStory(storyComponent);
 });
