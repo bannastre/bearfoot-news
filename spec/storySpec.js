9d172f14190500892551e85200961d99a3dacf24
RSijelmass <R.Sijelmass@gmail.com>
2017-06-24 09:40:10 +0100

    Unit test added for Story - has full text - test api extended with bodyText in fields

--- spec/storySpec.js
+++ spec/storySpec.js
@@ -2,7 +2,7 @@
 var assert = new Assert();
 
 
-var story = new Story({"id":"politics/live/2017/jun/23/merkel-says-mays-offer-on-eu-citizens-is-a-good-start-live-updates","type":"liveblog","sectionId":"politics","sectionName":"Politics","webPublicationDate":"2017-06-23T12:45:12Z","webTitle":"Police considering manslaughter charges over Grenfell Tower fire","webUrl":"https://www.theguardian.com/politics/live/2017/jun/23/merkel-says-mays-offer-on-eu-citizens-is-a-good-start-live-updates","apiUrl":"https://content.guardianapis.com/politics/live/2017/jun/23/merkel-says-mays-offer-on-eu-citizens-is-a-good-start-live-updates","fields":{"thumbnail":"https://media.guim.co.uk/9882b636d7e0df23cc792cb27de092decbebd8e9/0_30_4773_2865/500.jpg"},"isHosted":false});
+var story = new Story({"id":"politics/live/2017/jun/23/merkel-says-mays-offer-on-eu-citizens-is-a-good-start-live-updates","type":"liveblog","sectionId":"politics","sectionName":"Politics","webPublicationDate":"2017-06-23T12:45:12Z","webTitle":"Police considering manslaughter charges over Grenfell Tower fire","webUrl":"https://www.theguardian.com/politics/live/2017/jun/23/merkel-says-mays-offer-on-eu-citizens-is-a-good-start-live-updates","apiUrl":"https://content.guardianapis.com/politics/live/2017/jun/23/merkel-says-mays-offer-on-eu-citizens-is-a-good-start-live-updates","fields":{"thumbnail":"https://media.guim.co.uk/9882b636d7e0df23cc792cb27de092decbebd8e9/0_30_4773_2865/500.jpg", "bodyText": "Some testing text"},"isHosted":false});
 		
 function storyHasHeadline() {
 	var headline = "Police considering manslaughter charges over Grenfell Tower fire";
@@ -19,14 +19,20 @@ function storyHasUrl() {
 	assert.isEqual(story.url, url);
 }
 
+function storyHasFullText() {
+	var text = "Some testing text"
+	assert.isEqual(story.fullText, text)
+}
+
 function runUnitTests(){
-	console.log("Story: ")
+	//Story
 	storyHasHeadline();
   storyHasImage();
 	storyHasUrl();
-	console.log("Story Component: ")
+	storyHasFullText();
+	// Story Component
 	storyComponentHasStory();
-	console.log("Summary Component: ")
+	// Summary Component
 	summaryComponentHasUrl();
 	runsAylienApiCall();
   	console.log("Team Bearfoot: Unit Tests Passed");
